c05c70a1ea0f8c34f9e76b2cbfb2c0b9e1bd440b
Andrea <a.stirpe@onestein.nl>
2018-01-25 14:02:24 +0100

    [11.0][MIG] res_config_settings_enterprise_remove: port to V11

--- res_config_settings_enterprise_remove/__manifest__.py
+++ res_config_settings_enterprise_remove/__manifest__.py
@@ -1,18 +1,16 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 LasLabs Inc.
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Settings - Remove Enterprise Fields',
-    'summary': 'Remove fields in all settings views marked as enterprise',
-    'version': '10.0.1.0.0',
+    'summary': 'Remove enterprise options from settings',
+    'version': '11.0.1.0.0',
     'category': 'Maintenance',
-    'website': "https://laslabs.com",
+    'website': 'https://github.com/OCA/server-brand/',
     'author': 'LasLabs, Odoo Community Association (OCA)',
     'license': 'AGPL-3',
     'depends': [
         'base',
     ],
-    'application': False,
     'installable': True,
 }
