30b769aabd83d5a1788d92581ad5eb0f3a3373bd
Levent Karakaş <leventk@eskayazilim.com.tr>
2018-12-10 12:17:51 +0300

    Remove old module and fix travis

--- remove_odoo_enterprise/__manifest__.py
+++ remove_odoo_enterprise/__manifest__.py
@@ -1,5 +1,5 @@
 # Copyright 2018 Eska Yazılım ve Danışmanlık A.Ş (www.eskayazilim.com.tr)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Remove Odoo Enterprise',
     'summary': 'Remove enterprise modules and setting items',
