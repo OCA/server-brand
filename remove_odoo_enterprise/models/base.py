beab25e231c09087e796975f3086310108cbc969
Andrea <a.stirpe@onestein.nl>
2020-01-09 12:21:49 +0100

    [FIX] remove_odoo_enterprise: decorator for search method

--- remove_odoo_enterprise/models/base.py
+++ remove_odoo_enterprise/models/base.py
@@ -1,9 +1,13 @@
-from odoo import models
+# Copyright 2019-2020 Onestein (<https://www.onestein.eu>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import api, models
 
 
 class Base(models.AbstractModel):
     _inherit = "base"
 
+    @api.model
     def search(self, domain, offset=0, limit=None, order=None, count=False):
         if self._name == "payment.acquirer":
             domain += [("module_to_buy", "=", False)]
