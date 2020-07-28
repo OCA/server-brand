2ef3ac90edca123e7818b2b335d43ad5cadd5b61
Aleksandr Smirnov <smirnov.fl@yandex.ru>
2020-07-28 16:23:25 +0300

    MILC-191 prxgt_referral

--- remove_odoo_enterprise/models/base.py
+++ remove_odoo_enterprise/models/base.py
@@ -10,5 +10,6 @@ class Base(models.AbstractModel):
     @api.model
     def search(self, domain, offset=0, limit=None, order=None, count=False):
         if self._name == "payment.acquirer":
-            domain += [("module_to_buy", "=", False)]
+            buy_modules = self.env['ir.module.module'].sudo().search([('to_buy', '=', True)])
+            domain += [("module_id", "not in", buy_modules.ids)]
         return super().search(domain, offset, limit, order, count)
