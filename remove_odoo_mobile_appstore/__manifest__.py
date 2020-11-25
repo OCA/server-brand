05b2bb8f8825343920671b8a78639671103c84ee
Andrea <a.stirpe@onestein.nl>
2020-11-25 13:22:46 +0100

    [13.0][ADD] remove_odoo_mobile_appstore

---
+++ remove_odoo_mobile_appstore/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2020 Onestein (<https://www.onestein.eu>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Remove Odoo Mobile app store link",
+    "summary": "Remove the download Odoo mobile apps widget",
+    "version": "13.0.1.0.0",
+    "category": "Hidden",
+    "author": "Onestein, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/server-brand/",
+    "license": "AGPL-3",
+    "depends": ["base_setup"],
+    "data": ["views/res_config_settings_views.xml"],
+    "installable": True,
+}
