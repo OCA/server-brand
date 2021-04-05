c5e816e68ed8b4ba78d3e7495082461136e16fd1
Ivàn Todorovich <ivan.todorovich@gmail.com>
2021-04-05 11:53:39 -0300

    [MIG] portal_odoo_debranding: Migration to 14.0

--- portal_odoo_debranding/__manifest__.py
+++ portal_odoo_debranding/__manifest__.py
@@ -2,12 +2,12 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 {
     "name": "Remove Odoo Branding from Website",
-    "version": "12.0.1.0.0",
+    "version": "14.0.1.0.0",
     "development_status": "Beta",
     "category": "Hidden",
     "website": "https://github.com/OCA/server-brand",
     "author": "TAKOBI, Odoo Community Association (OCA)",
-    "maintainers": ["eLBati"],
+    "maintainers": ["eLBati", "ivantodorovich"],
     "license": "LGPL-3",
     "application": False,
     "installable": True,
