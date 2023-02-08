c7cf22d5139b4f0b006d201797b3c62ed6c098a5
remi-filament <30716308+remi-filament@users.noreply.github.com>
2023-02-08 12:04:59 +0100

    [MIG] remove_odoo_enterprise: Migration to 16.0

--- remove_odoo_enterprise/tests/test_remove_odoo_enterprise.py
+++ remove_odoo_enterprise/tests/test_remove_odoo_enterprise.py
@@ -1,5 +1,6 @@
 # Copyright 2020 Onestein (<http://www.onestein.eu>)
 # Copyright 2020 Akretion (<http://www.akretion.com>)
+# Copyright 2023 Le Filament (https://le-filament.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from lxml import etree
@@ -10,7 +11,7 @@ from odoo.tests import common
 class TestRemoveOdooEnterprise(common.TransactionCase):
     def test_res_config_settings(self):
         conf = self.env["res.config.settings"].create({})
-        view = conf.fields_view_get(view_type="form")
+        view = conf.get_views([[False, "form"]])["views"]["form"]
         doc = etree.XML(view["arch"])
 
         query = "//div[div[field[@widget='upgrade_boolean']]]"
@@ -18,8 +19,8 @@ class TestRemoveOdooEnterprise(common.TransactionCase):
             self.assertEqual(item.attrib["class"], "d-none")
 
     def test_search_base(self):
-        if self.env.get("payment.acquirer"):
-            acquirer_ids = self.env["payment.acquirer"].search([])
+        if self.env.get("payment.provider"):
+            acquirer_ids = self.env["payment.provider"].search([])
             self.assertFalse(any([a.module_to_buy for a in acquirer_ids]))
 
     def test_search_ir_module(self):
