4a6ce01bb697f64de23ae0fc8b7d146fd16533d7
PauMAVA <paumachetti@gmail.com>
2024-11-08 12:55:39 +0100

    [MIG] portal_odoo_debranding: Migration to 18.0

--- portal_debranding/__manifest__.py
+++ portal_debranding/__manifest__.py
@@ -2,7 +2,7 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 {
     "name": "Remove Odoo Branding from Website",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "development_status": "Beta",
     "category": "Hidden",
     "website": "https://github.com/OCA/server-brand",
