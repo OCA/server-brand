50b8364ffc13f9a157186264e883003ca99d3b24
Milan Topuzov <milan.topuzov@salewise.io>
2025-10-19 15:15:24 +0200

    [IMP] mail_debranding: pre-commit auto fixes

--- mail_debranding/__manifest__.py
+++ mail_debranding/__manifest__.py
@@ -14,7 +14,7 @@
     """,
     "version": "18.0.1.0.1",
     "category": "Social Network",
-    "website": "https://github.com/OCA/mail",
+    "website": "https://github.com/OCA/server-brand",
     "author": """Tecnativa, ForgeFlow, Onestein, Sodexis, Nexterp Romania,
              Odoo Community Association (OCA)""",
     "license": "AGPL-3",
